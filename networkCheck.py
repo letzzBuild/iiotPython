f7ea124ff0c231712f1c134c1c178bc3568df858
AnandLoginware <77340870+AnandLoginware@users.noreply.github.com>
2021-02-25 09:38:57 +0000

    Update networkCheck.py

--- networkCheck.py
+++ networkCheck.py
@@ -6,17 +6,20 @@ GPIO.setup(36,GPIO.OUT)
 
 
 def checkNetworkConnection():
+  flag = 0
   while(1):
      sleep(5)    
      result=subprocess.Popen("sudo mii-tool eth0 ",stdout=subprocess.PIPE,shell=True)
      finalResult=result.communicate()
      print(finalResult)
-     for i in finalResult:
-        if("eth0: no link" in i):
+     if (b'eth0: no link\n' in finalResult and flag == 0):
            GPIO.output(36,False)
            print("led off")
-        else:
+           flag = 1
+     elif (b'eth0: negotiated 100baseTx-FD flow-control, link ok\n' in finalResult and flag == 1):
            GPIO.output(36,True)
            print("led on")
-     break
+           flag = 0
+      else:
+           pass
 
